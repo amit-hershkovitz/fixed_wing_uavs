b51bc2af6e7d7c7ffa1d105eed21234e57474312
Amit Hershkovitz <amit.hershk@gmail.com>
2024-09-12 18:54:25 +0300

    First draft of a data_source class.

---
+++ src/data/data_source.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+from functools import partial
+
+from images_database import ImagesDatabase
+from extract_strategy import YoutubeDownloadStrategy
+
+
+class DataSource(ABC):
+    def __init__(self,
+                 database: ImagesDatabase,
+                 source_id: int,
+                 source_path: str,
+                 source_type: str,
+                 added: datetime,
+                 extract_strategy,
+                 transform_strategy,
+                 load_strategy):
+
+        self.database = database
+        self.source_id = source_id
+        self.source_path = source_path
+        self.source_type = source_type
+        self.added = added
+
+        self._extract_strategy = extract_strategy
+        self._transform_strategy = transform_strategy
+        self._load_strategy = load_strategy
+
+    def extract(self, destination):
+        self._extract_strategy.extract(self.source_path, destination)
+
+    def transform(self, destination, *paths):
+        self._transform_strategy.transform()
+
+    def load(self):
+        self._load_strategy.load()
+
+
+VideoSource = partial(DataSource,
+                      extract_stretegy=YoutubeDownloadStrategy,
+                      )
