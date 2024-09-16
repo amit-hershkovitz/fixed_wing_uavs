c7c8d4a9acbd344b87bd7f3ca5e8864e13599c9e
Amit Hershkovitz <amit.hershk@gmail.com>
2024-09-16 22:31:47 +0300

    Managing database transactions with context managers now + executing commits.

--- src/data/images_database.py
+++ src/data/images_database.py
@@ -6,63 +6,72 @@ class ImagesDatabase:
     def __init__(self, root_folder: str, database_name: str):
         self.root_folder = root_folder
         self.database_name = database_name
-        self.connection = sqlite3.connect(f'{root_folder}/{database_name}.db')
-        self.cursor = self.connection.cursor()
 
         self._create_images_table()
         self._create_data_sources_table()
 
     def add_source(self, source_path, source_type):
         add_time = time.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute(f"""
-                            INSERT INTO DataSources (source_path, source_type, added)
-                            VALUES ('{source_path}', '{source_type}', '{add_time}')
-                            """
-                            )
+        with sqlite3.connect(f'{self.root_folder}/{self.database_name}.db') as conn:
+            conn.cursor().execute(f"""
+                                INSERT INTO DataSources (source_path, source_type, added)
+                                VALUES ('{source_path}', '{source_type}', '{add_time}')
+                                """
+                                  )
+            conn.commit()
 
     def create_empty_image_entry(self) -> int:
         create_time = time.strftime('%Y-%m-%d %H:%M:%S')
-        self.cursor.execute(f"""
-                            INSERT INTO Images (source_path, source_type, created)
-                            VALUES (NULL, NULL, '{create_time}')
-                            """
-                            )
+        with sqlite3.connect(f'{self.root_folder}/{self.database_name}.db') as conn:
+            conn.cursor().execute(f"""
+                                INSERT INTO Images (image_path, source_id, created)
+                                VALUES (NULL, NULL, '{create_time}')
+                                """
+                                  )
+            conn.commit()
 
-        image_id = self.cursor.lastrowid
-
-        return image_id
+        return conn.cursor().lastrowid
 
     def insert_image(self, image_id: int, image_path: str, source_type: str):
-        self.cursor.execute(f"""
-                            UPDATE Images 
-                            SET source_path = '{image_path}'
-                                source_type = '{source_type}'
-                            WHERE image_id = {image_id}
-                            """
-                            )
+
+        with sqlite3.connect(f'{self.root_folder}/{self.database_name}.db') as conn:
+            conn.cursor().execute(f"""
+                                UPDATE Images 
+                                SET source_path = '{image_path}'
+                                    source_type = '{source_type}'
+                                WHERE image_id = {image_id}
+                                """
+                                )
+            conn.commit()
 
     def _create_data_sources_table(self):
-        self.cursor.execute("""
-                            CREATE TABLE IF NOT EXISTS DataSources (
-                                source_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                source_path TEXT UNIQUE,
-                                source_type TEXT,
-                                added DATETIME
-                            )
-                            """
-                            )
+        with sqlite3.connect(f'{self.root_folder}/{self.database_name}.db') as conn:
+            conn.cursor().execute("""
+                                CREATE TABLE IF NOT EXISTS DataSources (
+                                    source_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    source_path TEXT UNIQUE,
+                                    source_type TEXT,
+                                    added DATETIME
+                                )
+                                """
+                                )
+            conn.commit()
 
     def _create_images_table(self):
-        self.cursor.execute("""
-                            CREATE TABLE IF NOT EXISTS Images (
-                                image_id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                image_path TEXT UNIQUE,
-                                created DATETIME,
-                                source_id INTEGER
-                            )
-                            """
-                            )
+        with sqlite3.connect(f'{self.root_folder}/{self.database_name}.db') as conn:
+            conn.cursor().execute("""
+                                CREATE TABLE IF NOT EXISTS Images (
+                                    image_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    image_path TEXT UNIQUE,
+                                    created DATETIME,
+                                    source_id INTEGER
+                                )
+                                """
+                                )
+            conn.commit()
 
 
 if __name__ == '__main__':
     db = ImagesDatabase('/home/amit/codes/projects/python/fixed_wing_uavs/data', 'database')
+    id = db.create_empty_image_entry()
+    print(id)
